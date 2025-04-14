31d7827d301142c82bf8a6cdb250fe3bcfa0202f
Cupi33 <sufisaifuz0333@gmail.com>
2025-04-14 22:28:30 +0800

    Buang github dan google dari folder

--- src/views/customer/Login.js
+++ src/views/customer/Login.js
@@ -1,8 +1,6 @@
 import {
   Button,
   Card,
-  CardHeader,
-  CardBody,
   FormGroup,
   Form,
   Input,
@@ -18,51 +16,6 @@ const Login = () => {
     <>
       <Col lg="5" md="7">
         <Card className="bg-secondary shadow border-0">
-          <CardHeader className="bg-transparent pb-5">
-            <div className="text-muted text-center mt-2 mb-3">
-              <small>Sign in with</small>
-            </div>
-            <div className="btn-wrapper text-center">
-              <Button
-                className="btn-neutral btn-icon"
-                color="default"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <span className="btn-inner--icon">
-                  <img
-                    alt="..."
-                    src={
-                      require("../../assets/img/icons/common/github.svg")
-                        .default
-                    }
-                  />
-                </span>
-                <span className="btn-inner--text">Github</span>
-              </Button>
-              <Button
-                className="btn-neutral btn-icon"
-                color="default"
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <span className="btn-inner--icon">
-                  <img
-                    alt="..."
-                    src={
-                      require("../../assets/img/icons/common/google.svg")
-                        .default
-                    }
-                  />
-                </span>
-                <span className="btn-inner--text">Google</span>
-              </Button>
-            </div>
-          </CardHeader>
-          <CardBody className="px-lg-5 py-lg-5">
-            <div className="text-center text-muted mb-4">
-              <small>Or sign in with credentials</small>
-            </div>
             <Form role="form">
               <FormGroup className="mb-3">
                 <InputGroup className="input-group-alternative">
@@ -111,7 +64,6 @@ const Login = () => {
                 </Button>
               </div>
             </Form>
-          </CardBody>
         </Card>
         <Row className="mt-3">
           <Col xs="6">
