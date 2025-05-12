a84ac3e526eeab60aa2df872a281c9e4cf34b170
Cupi33 <sufisaifuz0333@gmail.com>
2025-05-13 00:42:14 +0800

    siapkan backend api untuk insert newborn table dah test dekat postman

---
+++ backend/routes/Application/Newborn.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import { execute } from "../../config/db.js";
+
+const router = express.Router();
+
+router.post('/1', async (req, res) => {
+  const {citizenID, fatherID,motherID,babyName,gender,dob,religion,race,address } = req.body;
+
+  try {
+    // Step 1: Get next appID from application sequence
+    const appIDResult = await execute(`SELECT application_seq.NEXTVAL AS appID FROM dual`);
+    const appID = appIDResult.rows[0].APPID;
+
+    // Step 2: Insert into application table
+    await execute(
+      `INSERT INTO application (appID, citizenID, appType, appDate)
+       VALUES (:1, :2, 'NEWBORN', SYSDATE)`,
+      [appID, citizenID]
+    );
+
+    // Step 3: Get next nbAppID from newborn_application sequence
+    const nbAppIDResult = await execute(`SELECT newborn_application_seq.NEXTVAL AS nbAppID FROM dual`);
+    const nbAppID = nbAppIDResult.rows[0].NBAPPID;
+
+    // Step 4: Insert into newborn_application with its own nbAppID, and link to appID
+    await execute(
+  `INSERT INTO NEWBORN_APPLICATION(nbAppID, APPID, fatherID, motherID,
+    babyName, gender, date_of_birth, religion, race, address)
+   VALUES (:1, :2, :3, :4, :5, :6, TO_DATE(:7, 'YYYY-MM-DD'), :8, :9, :10)`,
+  [nbAppID, appID, fatherID, motherID, babyName, gender,
+    dob, religion, race, address]
+);
+
+    // Step 5: Commit
+    await execute('COMMIT');
+
+    // Step 6: Send success response
+    res.status(201).json({
+      success: true,
+      message: 'Newborn Application sent successfully',
+      application: {
+        appID,
+        nbAppID
+      }
+    });
+
+  } catch (err) {
+    console.error('Application error:', err);
+    res.status(500).json({ success: false, message: 'Server error' });
+  }
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import authRoutes from './routes/authority/auth.js'
 import profileRoutes from './routes/profile/profileDisplay.js'
 import generalStat from './routes/stats/generalStat.js'
 import ICApplication from './routes/Application/ICApplication.js'
+import NewbornApplication from './routes/Application/Newborn.js'
 
 const app = express();
 app.use(cors());
@@ -13,6 +14,7 @@ app.use('/profile', profileRoutes);
 app.use('/',authRoutes);
 app.use('/stat',generalStat);
 app.use('/icapply',ICApplication);
+app.use('/newbornapply',NewbornApplication);
 
 const PORT = 5000;
 app.listen(PORT, () => {
