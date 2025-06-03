cd0ed1a07ab48f5eac38bd94a09cb56f01cb9601
Cupi33 <sufisaifuz0333@gmail.com>
2025-06-03 22:39:05 +0800

    fix AdminNavbar so it route dalam ui admin

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -1,27 +1,87 @@
-
 import { Link } from "react-router-dom";
 // reactstrap components
+//react
 import {
+  UncontrolledCollapse,
+  NavbarBrand,
   Navbar,
+  NavItem,
+  NavLink,
   Nav,
   Container,
- 
+  Row,
+  Col,
 } from "reactstrap";
 
-const AdminNavbar = (props) => {
+const AdminNavbar = () => {
   return (
     <>
-      <Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main">
-        <Container fluid>
-          <Link
-            className="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block"
-            to="/"
-          >
-            {props.brandText}
-          </Link>
-          <Nav className="align-items-center d-none d-md-flex" navbar>
-            
-          </Nav>
+      <Navbar className="navbar-top navbar-horizontal navbar-dark" expand="md">
+        <Container className="px-4">
+          <NavbarBrand to="/" tag={Link}>
+            <img
+              alt="..."
+              src={require("../../assets/img/icons/common/logo.png")}
+              style={{ height: "120px" }}
+            />
+          </NavbarBrand>
+          <button className="navbar-toggler" id="navbar-collapse-main">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <UncontrolledCollapse navbar toggler="#navbar-collapse-main">
+            <div className="navbar-collapse-header d-md-none">
+              <Row>
+                <Col className="collapse-brand" xs="6">
+                  <Link to="/authCitizen/MenuUtama">
+                    <img
+                      alt="..."
+                      src={require("../../assets/img/icons/common/logo.png")}
+                    />
+                  </Link>
+                </Col>
+                <Col className="collapse-close" xs="6">
+                  <button className="navbar-toggler" id="navbar-collapse-main">
+                    <span />
+                    <span />
+                  </button>
+                </Col>
+              </Row>
+            </div>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink className="nav-link-icon" to="/adminApplication/checkApplication" tag={Link}>
+                  <i className="ni ni-planet" />
+                  <span className="nav-link-inner--text">Semak Permohonan</span>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className="nav-link-icon"
+                  to="/adminMenu/laporan"
+                  tag={Link}
+                >
+                  <i className="ni ni-single-02" />
+                  <span className="nav-link-inner--text">Laporan</span>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link-icon" to="/authAdmin/loginAdmin" tag={Link}>
+                  <i className="ni ni-key-25" />
+                  <span className="nav-link-inner--text">Log Keluar</span>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  className="nav-link-icon"
+                  to="/citizenMenu/user-profile"
+                  tag={Link}
+                >
+                  <i className="ni ni-single-02" />
+                  <span className="nav-link-inner--text">Profil Pengguna</span>
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </UncontrolledCollapse>
         </Container>
       </Navbar>
     </>
